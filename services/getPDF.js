01c370922831a5994effd73a4809791b7272964c
LY <ly.jun.ming@gmail.com>
2025-11-03 16:00:39 +0700

    feat/ui and structure

---
+++ services/getPDF.js
@@ -0,0 +1,8 @@
+
+//reference: https://www.geeksforgeeks.org/html/how-to-generate-pdf-file-using-jspdf-library/
+
+async function generatePDF() {
+    const { jsPDF } = window.jspdf;
+    const doc = new jsPDF();       
+    //get the content of the response
+}
